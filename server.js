b5f02fb35f17e9c24e1431ec0cd5bfe01176730c
Prakash Rajendran <prakash.rajendran@capgemini.com>
2017-03-22 16:49:29 +0530

    image display on html dom

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ var server = require('./app');
 var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
-  // eslint-disable-next-line
   console.log('Server running on port: %d', port);
 });
